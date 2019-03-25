991cd3cddf21a2fbe1af4a1558cf5b7d61d6c16f
Emin Qasimov <eminqasimov@outlook.com>
2019-03-26 00:26:47 +0400

    added reload function when clicked

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+window.onclick = function(e) {
+  ReactDOM.unmountComponentAtNode(rootElement);
+	setTimeout(() => {
+      ReactDOM.render(<App/>, rootElement);
+  }, 300);
+} 
+
+ReactDOM.render(<App/>, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
