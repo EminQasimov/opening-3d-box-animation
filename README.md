737605630afeff94003f50da17f250048cd2b491
EminQasimov <eminqasimov@outlook.com>
2019-06-07 23:38:47 +0400

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-### Demo
-[CodePen - Opening 3D Cardboard Box Animation](https://codepen.io/eminqasimov/full/QoJOKY)
-
-[![ezgif-com-video-to-gif.gif](https://i.postimg.cc/mr0XmTfQ/ezgif-com-video-to-gif.gif)](https://postimg.cc/4KQQdk3d)
---
+++ precache-manifest.8cdfbf4640e392ec52b012163c3eea5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2c1a09aa9b474f7e353",
+    "url": "/EminQasimov/opening-3d-box-animation/static/js/runtime~main.1b47991d.js"
+  },
+  {
+    "revision": "9eae53fdfe9b48995560",
+    "url": "/EminQasimov/opening-3d-box-animation/static/js/main.4e52a245.chunk.js"
+  },
+  {
+    "revision": "40500d94abdce117fb20",
+    "url": "/EminQasimov/opening-3d-box-animation/static/js/2.0b193c9a.chunk.js"
+  },
+  {
+    "revision": "9eae53fdfe9b48995560",
+    "url": "/EminQasimov/opening-3d-box-animation/static/css/main.6de01566.chunk.css"
+  },
+  {
+    "revision": "d87f89aae3309f42688f5880c7590f81",
+    "url": "/EminQasimov/opening-3d-box-animation/index.html"
+  }
+];
